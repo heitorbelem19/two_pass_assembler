2f71d0dd3d4a556325cba744a6bbada2f6be433a
Heitor Belem <heitorbelem21@gmail.com>
2020-10-06 21:21:02 -0300

    Write preprocessor file

--- src/montador.cpp
+++ src/montador.cpp
@@ -10,17 +10,28 @@ int main(int argc, char** argv){
   */
   io_manager file_input(argv[2]);
   std::vector<std::string> uploaded_file = file_input.get_uploaded_file();
-
-  // Faz o pre processamento do arquivo .asm
-  pre_processor pre_processamento;
-  pre_processamento.process(uploaded_file);
-  for(int i=0; i<uploaded_file.size(); i++){
-    std::cout << uploaded_file[i] << '\n';
+  
+  if(strcmp(argv[1], "-p") == 0){
+    // Faz o pre processamento do arquivo .asm
+    pre_processor pre_processamento;
+    pre_processamento.process(uploaded_file);
+    std::ofstream pre_process_file;
+    pre_process_file.open("montadorpre.txt");
+    for(int i=0; i<uploaded_file.size(); i++){
+      pre_process_file << uploaded_file[i] << '\n';
+    }
+    pre_process_file.close();
   }
-  std::cout << "--------------------" << std::endl;
-  assembler montador;
-  montador.first_pass(uploaded_file);
-  montador.second_pass(uploaded_file);
+  else if(strcmp(argv[1], "-o") == 0){
+    pre_processor pre_processamento;
+    pre_processamento.process(uploaded_file);
+    assembler montador;
+    montador.first_pass(uploaded_file);
+    montador.second_pass(uploaded_file);
+  }
+  else{
+    std::cout << "Diretiva invalida\n";
+  }
   
   return 0;
 }
